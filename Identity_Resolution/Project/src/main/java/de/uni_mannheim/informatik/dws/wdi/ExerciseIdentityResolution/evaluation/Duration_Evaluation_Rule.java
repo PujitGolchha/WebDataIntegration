7819a77af7783ac56d4e031395ff6da76b1a8a77
abhay-joseph <78967926+abhay-joseph@users.noreply.github.com>
2022-12-01 19:24:55 +0100

    Tempo_fuser changed from avg to max, duration fuser and evaluation created

---
+++ Identity_Resolution/Project/src/main/java/de/uni_mannheim/informatik/dws/wdi/ExerciseIdentityResolution/evaluation/Duration_Evaluation_Rule.java
@@ -0,0 +1,32 @@
+package de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.evaluation;
+
+import de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.model.Song;
+import de.uni_mannheim.informatik.dws.winter.datafusion.EvaluationRule;
+import de.uni_mannheim.informatik.dws.winter.model.Correspondence;
+import de.uni_mannheim.informatik.dws.winter.model.Matchable;
+import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
+
+
+public class Duration_Evaluation_Rule extends EvaluationRule<Song, Attribute> {
+    @Override
+    public boolean isEqual(Song record1, Song record2, Attribute schemaElement) {
+
+        if(record1.getDuration() == -1 && record2.getDuration() == -1)
+            return true;
+        else if(Math.abs(record1.getDuration() - record2.getDuration()) <= 3)
+            return true;
+        else
+            return false;
+    }
+
+    /* (non-Javadoc)
+     * @see de.uni_mannheim.informatik.wdi.datafusion.EvaluationRule#isEqual(java.lang.Object, java.lang.Object, de.uni_mannheim.informatik.wdi.model.Correspondence)
+     */
+    @Override
+    public boolean isEqual(Song record1, Song record2,
+                           Correspondence<Attribute, Matchable> schemaCorrespondence) {
+        return isEqual(record1, record2, (Attribute)null);
+    }
+
+}
+
--- Identity_Resolution/Project/src/main/java/de/uni_mannheim/informatik/dws/wdi/ExerciseIdentityResolution/fusers/Duration_Fuser.java
+++ Identity_Resolution/Project/src/main/java/de/uni_mannheim/informatik/dws/wdi/ExerciseIdentityResolution/fusers/Duration_Fuser.java
@@ -3,8 +3,6 @@ package de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.fusers;
 import de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.conflictresolution.numeric.Max;
 import de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.model.Song;
 import de.uni_mannheim.informatik.dws.winter.datafusion.AttributeValueFuser;
-import de.uni_mannheim.informatik.dws.winter.datafusion.conflictresolution.numeric.Average;
-import de.uni_mannheim.informatik.dws.winter.datafusion.conflictresolution.string.ShortestString;
 import de.uni_mannheim.informatik.dws.winter.model.Correspondence;
 import de.uni_mannheim.informatik.dws.winter.model.FusedValue;
 import de.uni_mannheim.informatik.dws.winter.model.Matchable;
@@ -12,24 +10,24 @@ import de.uni_mannheim.informatik.dws.winter.model.RecordGroup;
 import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
 import de.uni_mannheim.informatik.dws.winter.processing.Processable;
 
-public class Tempo_Fuser extends AttributeValueFuser<Double, Song, Attribute>  {
+public class Duration_Fuser extends AttributeValueFuser<Double, Song, Attribute>  {
 
-    public Tempo_Fuser () {
+    public Duration_Fuser() {
         super(new Max<Song, Attribute>());
     }
     @Override
     public boolean hasValue(Song record, Correspondence<Attribute, Matchable> correspondence) {
-        return record.hasValue(Song.Tempo);
+        return record.hasValue(Song.Duration);
     }
     @Override
     public Double getValue(Song record, Correspondence<Attribute, Matchable> correspondence) {
-        return  (double)record.getTempo();
+        return  (double)record.getDuration();
     }
     @Override
     public void fuse(RecordGroup<Song, Attribute> group, Song fusedRecord, Processable<Correspondence<Attribute, Matchable>> schemaCorrespondences, Attribute schemaElement) {
         FusedValue<Double, Song, Attribute> fused = getFusedValue(group, schemaCorrespondences, schemaElement);
-        fusedRecord.setTempo(fused.getValue().floatValue());
-        fusedRecord.setAttributeProvenance(Song.Tempo,
+        fusedRecord.setDuration(fused.getValue().intValue());
+        fusedRecord.setAttributeProvenance(Song.Duration,
                 fused.getOriginalIds());
     }
 }
